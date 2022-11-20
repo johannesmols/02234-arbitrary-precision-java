2d7816fb5d7f7daf09772308138ff2f2e43e8b91
tabs-of-course <juriborren+tabs-of-course@gmail.com>
2022-11-20 05:26:31 +0100

    fixed addition. It will now respect any base

--- src/mols/johannes/Main.java
+++ src/mols/johannes/Main.java
@@ -1,24 +1,39 @@
 package mols.johannes;
 
+import java.util.Arrays;
+
 public class Main {
 
-    public static long BASE = (2L << 62L) - 1L;
+    public static long BASE =(2L << 30L) - 1;
 
     public static void main(String[] args) {
         // In Base 10: 123456789123456789123456789123456789123456789123456789123456789
-        BigInteger a = new BigInteger(new long[] { 497017313360941710L, 8371747259953841529L, 3778830014700828830L, 157342L });
+        //64-bit
+//        BigInteger a = new BigInteger(new long[] { 497017313360941710L, 8371747259953841529L, 3778830014700828830L, 157342L });
+        //32-bit
+        BigInteger a = new BigInteger(new long[] {2027931598, 434255641, 566416197, 1590781320, 100546664, 603720888, 1258739});
+//        System.out.println(Arrays.toString(a.digits));
         // In Base 10: 987654321987654321987654321987654321987654321987654321
-        BigInteger b = new BigInteger(new long[] { 4261302582211177503L, 3381573805932972480L, 11609820760617042L });
+
+//        BigInteger b = new BigInteger(new long[] { 4261302582211177503L, 3381573805932972480L, 11609820760617042L });
+        BigInteger b = new BigInteger(new long[] { 811174784, 1389239218, 600952680, 1825296399, 802200602, 21624976 });
+//        System.out.println(Arrays.toString(b.digits));
 
         // Test addition (Result in Base 10: 123456790111111111111111111111111111111111111111111111111111110)
         BigInteger added = Addition.Add(a, b);
-        BigInteger correctResult = new BigInteger(new long[] { 4758319895572119213L, 2529949029032038202L, 3790439835461445873L, 157342L });
-        System.out.println("Addition correct: " + added.equals(correctResult));
+        System.out.println(Arrays.toString(added.digits));
 
+//        BigInteger correctResult = new BigInteger(new long[] { 4758319895572119213L, 2529949029032038202L, 3790439835461445873L, 157342L});
+        BigInteger correctResult = new BigInteger(new long[] { 691622735, 1823494860, 1167368877, 1268594072, 902747267, 625345864, 1258739 });
+//        System.out.println("Addition correct: " + added.equals(correctResult));
+        System.out.println(Arrays.toString(correctResult.digits));
+
+//        BigInteger multiplied = Multiplication.Multiply(a,b);
+//        System.out.println(Arrays.toString(multiplied.digits));
         // Test short division (Result in Base 10: )
-        BigInteger oneDigitDividend = new BigInteger(new long[] { 1337L });
-        BigInteger divided = Division.Divide(a, oneDigitDividend);
-        BigInteger correctShortDivisionResult = new BigInteger(new long[] { });
-        System.out.println("Short division correct: " + divided.equals(correctResult));
+//        BigInteger oneDigitDividend = new BigInteger(new long[] { 1337L });
+//        BigInteger divided = Division.Divide(a, oneDigitDividend);
+//        BigInteger correctShortDivisionResult = new BigInteger(new long[] { });
+//        System.out.println("Short division correct: " + divided.equals(correctResult));
     }
 }
---
+++ src/mols/johannes/Multiplication.java
@@ -0,0 +1,61 @@
+package mols.johannes;
+
+public class Multiplication {
+    public static BigInteger Multiply(BigInteger a, BigInteger b) {
+        // Temporary variables for addition
+        long carry;
+        long largest_size;
+        long res_size;
+        long a_val;
+        long b_val;
+        long tmp_res;
+
+        // Temporary values for looping, reading in values, etc.
+        long i = 0;
+        long j = 0;
+
+        // Addition of A and B
+        carry = 0;
+        largest_size = ((a.digits.length) >= (b.digits.length)) ? a.digits.length : b.digits.length;
+        // If there is no carry at the end, the array unfortunately still gets an additional element
+        // Can't resize arrays, so we would have to check at the end and copy the values to a new array
+//        long[] digits_res = new long[(int) largest_size + 1];
+        res_size = (a.digits.length) + (b.digits.length);
+        long[] digits_res = new long[(int) res_size];
+        while (i < res_size) {
+            // Check if A has more digits
+            if (i < (a.digits.length)) {
+                a_val = a.digits[(int) i];
+            } else {
+                a_val = 0;
+            }
+
+            // Check if B has more digits
+            if (i < (b.digits.length)) {
+                b_val = b.digits[(int) i];
+            } else {
+                b_val = 0;
+            }
+
+            tmp_res = (a_val + b_val) + carry;
+            if ((tmp_res >= (Main.BASE + 1)) && (tmp_res < 0)) { // base + 1 gets the minimum possible value (in negative space)
+                // Overflow occurred
+                digits_res[(int) i] = (tmp_res + Main.BASE) + 2; // Overflow goes in negative numbers, so put it back into positive space
+                carry = 1; // Set carry to 1
+            } else {
+                digits_res[(int) i] = tmp_res;
+                carry = 0;
+            }
+            i++;
+        }
+
+        if (carry > 0) {
+            digits_res[(int) largest_size] = carry;
+        } else {
+            // Accessing this index will otherwise throw a runtime exception (uninitialized)
+            digits_res[(int) largest_size] = 0;
+        }
+
+        return new BigInteger(digits_res);
+    }
+}
